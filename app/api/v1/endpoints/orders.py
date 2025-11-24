439845f81b4126d06aca888944a35eea40bd3d44
nafiMulyo <muhamadnafimulyo@gmail.com>
2025-11-24 19:31:48 +0700

    adds notification after push it to db

--- app/api/v1/endpoints/orders.py
+++ app/api/v1/endpoints/orders.py
@@ -3,14 +3,28 @@ from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.crud import crud_order
 from app.schemas.order import OrderCreate, OrderResponse
+from app.websockets.manager import manager 
+import json
 
 router = APIRouter()
 
 @router.post("/orders", response_model=OrderResponse)
-def create_new_order(order: OrderCreate, db: Session = Depends(get_db)):
+async def create_new_order(order: OrderCreate, db: Session = Depends(get_db)):
     """
     Create new order with items.
     Server will calculate prices, tax, and total.
     Stock will be reduced automatically.
     """
-    return crud_order.create_order(db=db, order_in=order)
+    new_order = crud_order.create_order(db=db, order_in=order)
+    
+    notification_data = {
+        "event": "NEW_ORDER",
+        "order_id": str(new_order.id),
+        "bill_name": new_order.bill_name,
+        "total": float(new_order.grand_total) 
+    }
+
+    await manager.broadcast(notification_data)
+    
+    return new_order
+
