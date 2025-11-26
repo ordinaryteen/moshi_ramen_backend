8e6244128eca7b48fd21ee57ff41ae2020729d7b
nafiMulyo <muhamadnafimulyo@gmail.com>
2025-11-26 19:52:13 +0700

    enable CORS

--- app/main.py
+++ app/main.py
@@ -3,8 +3,17 @@ from app.core.config import settings
 from app.api.v1.api import api_router
 from fastapi import WebSocket, WebSocketDisconnect
 from app.websockets.manager import manager
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Development: Boleh dari semua domain. Production: Ganti jadi ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
@@ -21,5 +30,7 @@ async def websocket_endpoint(websocket: WebSocket):
             data = await websocket.receive_text()
     except WebSocketDisconnect:
         manager.disconnect(websocket)
+
+
 
 
