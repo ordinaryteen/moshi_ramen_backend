c4b9584017b3d8debd115f68b1f99a9b619001b4
nafiMulyo <muhamadnafimulyo@gmail.com>
2025-11-26 16:51:31 +0700

    adds authenticated login endpoints

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -3,5 +3,6 @@ from app.api.v1.endpoints import products, orders
 
 api_router = APIRouter()
 
+api_router.include_router(auth.router, tags=["Authentication"])
 api_router.include_router(products.router, tags=["Products & Categories"])
 api_router.include_router(orders.router, tags=["Orders"]) 
---
+++ app/api/v1/endpoints/auth.py
@@ -0,0 +1,35 @@
+from typing import Any
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from app.core.database import get_db
+from app.core.security import create_access_token, verify_password
+from app.models.user import User
+from app.schemas.token import Token
+
+router = APIRouter()
+
+@router.post("/login", response_model=Token)
+def login_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
+    """
+    OAuth2 compatible token login, get an access token for future requests.
+    """
+    user = db.query(User).filter(User.username == form_data.username).first()
+    
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    if not user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+
+    # 4. Bikin Access Token
+    access_token = create_access_token(data={"sub": user.username, "id": user.id})
+    
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+    }
